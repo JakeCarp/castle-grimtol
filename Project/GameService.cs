808fd1a99b2e1958e1949c49f5138edf840dbc7a
Jake Carpenter <jakecarpenter407@gmail.com>
2019-01-10 14:46:01 -0700

    engine is mostly done

--- Project/GameService.cs
+++ Project/GameService.cs
@@ -12,6 +12,130 @@ namespace CastleGrimtol.Project
     public bool Playing { get; set; }
 
 
+      #endregion
+      //creating items
+      #region 
+      Item wornHammer = new Item("Worn Hammer", "A very old hammer that your father gave you. It's seen more than it's fair share of work, but it hasn't failed you yet.");
+
+      #endregion
+      //adding Exits
+      #region 
+      constructionSite.Exits.Add("east", wreckedClearing);
+      wreckedClearing.Exits.Add("west", constructionSite);
+      wreckedClearing.Exits.Add("south", smallBunkerEntrance);
+      smallBunkerEntrance.Exits.Add("north", wreckedClearing);
+      smallBunkerEntrance.Exits.Add("down", hallway1);
+      hallway1.Exits.Add("up", smallBunkerEntrance);
+      hallway1.Exits.Add("south", storage1);
+      hallway1.Exits.Add("southwast", securityCheck1);
+      hallway1.Exits.Add("east", hallway2);
+      storage1.Exits.Add("north", hallway1);
+      securityCheck1.Exits.Add("northwest", hallway1);
+      securityCheck1.Exits.Add("southeast", hallway2);
+      hallway2.Exits.Add("west", hallway1);
+      hallway2.Exits.Add("southwest", securityCheck1);
+
+      #endregion
+      //adding items
+      #region 
+      constructionSite.Items.Add(wornHammer);
+      #endregion
+      //build item use dictionary
+      #region 
+      itemUseGuide = new Dictionary<string, string>();
+      itemUseGuide.Add("worn hammer-lock", "thing happens");
+      itemUseGuide.Add("worn hammer-jack", "thing happens");
+      #endregion
+      CurrentRoom = constructionSite;
+    }
+    public gameActions Parse(string text)
+    {
+      string input = text.ToLower();
+      if (input.Contains("look"))
+      {
+        return gameActions.lookRoom;
+      };
+      //movement
+      #region 
+      if (input.Contains("go") && input.Contains("north"))
+      {
+        return gameActions.goNorth;
+      };
+      if (input.Contains("go") && input.Contains("east"))
+      {
+        return gameActions.goEast;
+      };
+      if (input.Contains("go") && input.Contains("south"))
+      {
+        return gameActions.goSouth;
+      };
+      if (input.Contains("go") && input.Contains("west"))
+      {
+        return gameActions.goWest;
+      };
+      if (input.Contains("go") && input.Contains("down"))
+      {
+        return gameActions.goDown;
+      };
+      if (input.Contains("go") && input.Contains("up"))
+      {
+        return gameActions.goUp;
+      };
+      if (input.Contains("go") && input.Contains("northeast"))
+      {
+        return gameActions.goNorthEast;
+      };
+      if (input.Contains("go") && input.Contains("northwest"))
+      {
+        return gameActions.goNorthWest;
+      };
+      if (input.Contains("go") && input.Contains("southeast"))
+      {
+        return gameActions.goSouthEast;
+      };
+      if (input.Contains("go") && input.Contains("southwest"))
+      {
+        return gameActions.goSouthWest;
+      };
+      if (input.Contains("go") && input.Contains("stairs") && input.Contains("up"))
+      {
+        return gameActions.goUp;
+      }
+      if (input.Contains("go") && input.Contains("stairs") && input.Contains("down"))
+      {
+        return gameActions.goDown;
+      }
+      #endregion
+      //console commands
+      #region
+      if (input.Contains("help"))
+      {
+        return gameActions.help;
+      };
+      if (input.Contains("inventory"))
+      {
+        return gameActions.playerInventory;
+      };
+      if (input.Contains("quit"))
+      {
+        return gameActions.gameQuit;
+      };
+      if (input.Contains("reset"))
+      {
+        return gameActions.gameReset;
+      };
+      #endregion
+      if (input.Contains("use") && input.Contains("hammer") && input.Contains("lock"))
+      {
+        return gameActions.useHammerOnLock;
+      };
+      if (input.Contains("take") && input.Contains("hammer"))
+      {
+        return gameActions.takeHammer;
+      }
+
+      return gameActions.wait;
+    }
     public void GetUserInput()
     {
       //parsing user input
